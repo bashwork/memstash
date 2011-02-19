a379e9c0969dabe05a6ccc3c9778361713d06668
Galen Collins <bashwork@gmail.com>
2011-02-18 20:01:19 -0800

    cleaning up build

--- src/main/scala/org/stash/storage/RedisStorage.scala
+++ src/main/scala/org/stash/storage/RedisStorage.scala
@@ -1,18 +1,18 @@
-package org.stash.storage
-
-import net.rubyeye.xmemcached._
-
-/**
- * @summary
- */
-class RedisStorage(host:String, port:Int) extends StashStorage {
-    private val client:MemcachedClient = new XMemcachedClient(host, port)
-
-    override def get(key: String) : StashObject = client.get(key)
-    override def put(el: StashObject) = client.set(el.key, el.expire.toInt, el.bytes)
-    override def keys:List[String] = null//client.getKeyIterator()
-    override def remove(key: String) = client.delete(key)
-    override def size : Long = 0
-    override def clear = null
-    def this() = this("localhost", 12000)
-}
+//package org.stash.storage
+//
+//import net.rubyeye.xmemcached._
+//
+///**
+// * @summary
+// */
+//class RedisStorage(host:String, port:Int) extends StashStorage {
+//    private val client:MemcachedClient = new XMemcachedClient(host, port)
+//
+//    override def get(key: String) : StashObject = client.get(key)
+//    override def put(el: StashObject) = client.set(el.key, el.expire.toInt, el.bytes)
+//    override def keys:List[String] = null//client.getKeyIterator()
+//    override def remove(key: String) = client.delete(key)
+//    override def size : Long = 0
+//    override def clear = null
+//    def this() = this("localhost", 12000)
+//}
